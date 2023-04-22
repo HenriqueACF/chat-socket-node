1cfe0f0d2393767b0c3dc3a99cfac6923ad1e8c9
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-04-22 19:16:40 -0300

    feat: init project

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app)
+
+server.listen(3000)
+
+app.use(express.static(path.join(__dirname, 'public')))
