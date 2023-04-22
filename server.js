f48c6c72a1cfeba731f9abbc9870c4ffb441df33
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-04-22 20:00:41 -0300

    feat: created a simple chat interface

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ const app = express();
 const server = http.createServer(app)
 
 server.listen(3000)
+console.log('API RODANDO NA PORTA 3000')
 
 app.use(express.static(path.join(__dirname, 'public')))
