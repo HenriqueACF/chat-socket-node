760b974429ecd55fff88e2f69a595485c42a80a6
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-04-23 16:38:03 -0300

    feat: estabelecendo conexao do socket

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app)
+const io = socketIO(server)
 
 server.listen(3000)
 console.log('API RODANDO NA PORTA 3000')
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+//Conexao socket
+io.on('connection', (socket)=>{
+    console.log('Conextao encontrada...')
+
+
+})
