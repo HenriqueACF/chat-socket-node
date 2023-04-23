382e04b7a62f3c8a76f4f6c11b313118de6134de
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-04-23 17:05:09 -0300

    feat: entrando no chat

--- public/main.js
+++ public/main.js
@@ -1,1 +1,42 @@
 const socket = io()
+let username = ''
+let userList = []
+
+let loginPage = document.querySelector('#loginPage')
+let chatPage = document.querySelector('#chatPage')
+
+let loginInput = document.querySelector('#loginNameInput')
+let textInput = document.querySelector('#chatTextInput')
+
+loginPage.style.display = 'flex'
+chatPage.style.display = 'none'
+
+function renderUserList(){
+    let ul = document.querySelector('.userList')
+    ul.innerHTML = ''
+
+    userList.forEach(i=>{
+        ul.innerHTML += '<li>'+i+'</li>'
+    })
+}
+
+loginInput.addEventListener('keyup', (e)=>{
+    if(e.keyCode === 13){
+        let name = loginInput.value.trim()
+        if(name != ''){
+            username = name
+            document.title = 'Chat ('+username+')'
+
+            socket.emit('join-request', username)
+        }
+    }
+})
+
+socket.on('user-ok', (list)=>{
+      loginPage.style.display = 'none'
+    chatPage.style.display = 'flex'
+    textInput.focus()
+
+    userList = list
+    renderUserList()
+})
--- server.js
+++ server.js
@@ -12,9 +12,18 @@ console.log('API RODANDO NA PORTA 3000')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+let connectedUsers = []
+
 //Conexao socket
 io.on('connection', (socket)=>{
     console.log('Conextao encontrada...')
 
+    socket.on('join-request', (username)=>{
+        socket.username = username
+        connectedUsers.push(username)
 
+        console.log(connectedUsers)
+
+        socket.emit('user-ok', connectedUsers)
+    })
 })
